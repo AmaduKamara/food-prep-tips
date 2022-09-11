e5e289a02fb88f0fb04a77dd4c5e3124d7f046a2
Amkam <amaduskamara@gmail.com>
2022-09-11 23:28:34 +0000

    Style header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 // pages
-import Home from "./pages/Home"
-import Create from "./pages/Create"
-import Update from "./pages/Update"
-
+import Home from "./pages/Home";
+import Create from "./pages/Create";
+import Update from "./pages/Update";
 
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <h1>Food Prep Tips</h1>
-        <Link to="/">Home</Link>
-        <Link to="/create">Create New Food</Link>
+      <nav className="bg-cyan-500 py-4 shadow-xl">
+        <div className="container mx-auto flex justify-between items-center">
+          <h1 className="text-xl text-white">Food Prep Tips</h1>
+          <ul className="flex text-white">
+            <li className="mx-10">
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/create">Create New Food</Link>
+            </li>
+          </ul>
+        </div>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
