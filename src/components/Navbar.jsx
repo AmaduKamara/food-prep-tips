aae6c9d108d39e908eeb20d9fea546e59d0ee354
Amkam <amaduskamara@gmail.com>
2022-09-18 11:04:12 +0000

    Work on mobile responsiveness on the Header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,18 +4,18 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav className="bg-cyan-500 py-4 shadow-xl">
-      <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-xl text-white">Food Prep Tips</h1>
+      <div className="px-4 md:container md:mx-auto flex justify-between items-center">
+        <h1 className="text-xl md:text-2xl text-white font-thin">FoodPrep Tips</h1>
         <ul className="flex text-white">
-          <li className="mx-10">
+          <li className="mx-3 md:mx-10">
             <Link to="/">Home</Link>
           </li>
           <li>
             <Link
               to="/create"
-              className="border py-2 px-5 rounded-md text-cyan-500 bg-white hover:bg-cyan-500 hover:text-white transition-all ease-in-out duration-500"
+              className="border py-2 px-2 md:px-5 rounded-md text-cyan-500 bg-white hover:bg-cyan-500 hover:text-white transition-all ease-in-out duration-500"
             >
-              Create New Food
+              New Food
             </Link>
           </li>
         </ul>
