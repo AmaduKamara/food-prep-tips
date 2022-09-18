8e73518e0bae62d6a6d13db8dd94f386dcba80d7
Amkam <amaduskamara@gmail.com>
2022-09-18 11:12:50 +0000

    Edit card styles to have left border and changed star bg

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -1,6 +1,6 @@
 const FoodCard = ({ food }) => {
   return (
-    <div className="shadow-lg p-5 relative border rounded-md">
+    <div className="shadow-lg p-5 relative rounded-md border-l-4 border-cyan-400">
       <h3 className="text-lg font-semibold text-cyan-600">{food.title}</h3>
       <div className="my-3 w-full">
         <p className="text-gray-400 mb-1">Prep-Time: {food.preptime} minutes</p>
@@ -11,7 +11,7 @@ const FoodCard = ({ food }) => {
       <h5 className="font-semibold text-sm text-gray-500">
         Author: <span className="text-gray-400">{food.author}</span>
       </h5>
-      <div className="absolute top-0 right-0 -mt-4 bg-cyan-500 w-10 h-10 flex items-center justify-center text-white rounded-full shadow-lg">
+      <div className="absolute top-0 right-0 -mt-4 bg-teal-500 w-10 h-10 flex items-center justify-center text-white rounded-full shadow-lg">
         {food.rating}
       </div>
     </div>
