d33552e4f99d9de5675a7ec94d0059fa6226b2b3
Amkam <amaduskamara@gmail.com>
2023-07-22 11:29:11 +0000

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Hero() {
   return (
-    <section className="w-full h-36 md:h-56 bg-cyan-500 flex justify-center items-center text-center">
+    <section className="w-full h-36 md:h-56 bg-cyan-600 flex justify-center items-center text-center">
       <div className="px-6 md:container md:mx-auto">
         <h1 className="text-lg md:text-3xl text-white font-thin">
           “I know once people get connected to real food, they never change
